73b67b6da63eda8fad6fa2ee431733ba142179d6
cfahim880 <cfahim880@gmail.com>
2024-08-31 22:23:41 +0600

    changes done

--- coustomer/views.py
+++ coustomer/views.py
@@ -68,7 +68,7 @@ def activate(request,uid64,token):
     if user is not None and default_token_generator.check_token(user,token):
         user.is_active=True
         user.save()
-        return redirect("https://blossomcart.onrender.com/coustomer/login/")
+        return redirect("https://cfahim207.github.io/BlossomCart-Frontend/login.html")
     else:
         return redirect('register')
 
